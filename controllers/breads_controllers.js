c2ebda888b6bf3f4c6e261810910a7f9afc73eb3
Jagbir Mangat <jaguarsandhu@gmail.com>
2022-09-25 11:21:00 -0700

    delete route complete

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -21,13 +21,14 @@ breads.get('/new', (req, res) => {
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    if (Bread[req.params.arrayIndex]) {
-      res.render('Show', {
-        bread:Bread[req.params.arrayIndex]
-      })
-    } else {
-      res.send('404')
-    }
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
+    })
+  } else {
+    res.render('404')
+  }
 })
   
 // CREATE
@@ -44,6 +45,11 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 })
 
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
   
 
 
